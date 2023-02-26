4fff4a3643913587db5b945edb0a26d85c18f4b8
diomnne <dionnemaglasang@gmail.com>
2023-02-26 10:46:35 +0800

    c project

---
+++ decimaltobinary.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+long binary (int);
+
+int main()
+{
+    long bin;
+    int dec;
+
+    printf("Input any decimal number: ");
+    scanf("%d", &dec);
+
+    bin = binary(dec);
+    printf("The binary value is : %ld\n", bin);
+
+    return 0;
+}
+
+long binary (dec)
+{
+    long bin=0, r, f=1;
+    while (dec!=0)
+    {
+        r = dec%2;
+        bin = bin+r*f;
+        f = f*10;
+        dec = dec/2;
+    }
+    return bin;
+
+}
